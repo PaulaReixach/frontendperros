685248cdb18b23626927d515508268a8898741ad
PaulaReixach <paulirei@gmail.com>
2023-11-24 18:21:14 +0100

    Conexión entre back + front, més info a README

---
+++ src/app/elasticsearch.service.spec.ts
@@ -0,0 +1,54 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ElasticsearchService } from './elasticsearch.service';
+
+describe('ElasticsearchService', () => {
+  let service: ElasticsearchService;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ElasticsearchService],
+    });
+
+    service = TestBed.inject(ElasticsearchService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    // Verifica que no haya solicitudes pendientes después de cada prueba
+    httpTestingController.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should retrieve information from Elasticsearch', () => {
+    const mockResponse = { /* mock response data */ };
+
+    service.getInfo().subscribe((data) => {
+      expect(data).toEqual(mockResponse);
+    });
+
+    const req = httpTestingController.expectOne('http://localhost:9201/check');
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(mockResponse);
+  });
+
+  it('should perform a search in Elasticsearch', () => {
+    const query = 'example';
+    const mockResponse = { /* mock response data */ };
+
+    service.search(query).subscribe((data) => {
+      expect(data).toEqual(mockResponse);
+    });
+
+    const req = httpTestingController.expectOne(`http://localhost:9201/search?query=${query}`);
+    expect(req.request.method).toEqual('GET');
+
+    req.flush(mockResponse);
+  });
+});
---
+++ src/app/elasticsearch.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ElasticsearchService {
+  //Cada url va empezar por /api
+  private apiUrl = '/api';
+
+  constructor(private http: HttpClient) {}
+
+  //Query para pillar info por ejemplo /api/check (que se encuentra en el back)
+  getInfo(): Observable<any> {
+    // Cambia de GET a POST
+    return this.http.post<any>(`${this.apiUrl}/check`, {});
+  }
+
+  //Query para obtener un objeto /api/search+item (está en el back también)
+  search(query: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/search?query=${query}`);
+  }
+}
