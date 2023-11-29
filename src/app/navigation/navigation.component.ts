b026921fa7fe85dabde66027f37543fd7fbc0e00
paula <paulirei@gmail.com>
2023-11-29 01:06:14 +0100

    Creació de header + logo + navigation COMPONENTS Treballar als botons sobre de navigation.
    :)

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.css'],
+})
+export class NavigationComponent {
+  @Input() dropdownOpen: boolean = false;
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  closeMenu() {
+    this.dropdownOpen = false;
+  }
+
+}
