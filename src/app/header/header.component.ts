1cfa9425364d388f89fe694543cd8c00c65a4857
paula <paulirei@gmail.com>
2023-12-01 16:48:32 +0100

    Nova barra de search + lupa interactiva  - Es guarden els valors en una variable: terminosDeBusqueda
     - Intentaré millorar l'angular a nova versio a ver si puc utilitzar chips, que sino no quedará bé :(

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  terminosDeBusqueda: string[] = [];
+
+
+  buscarPorTecla(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      const inputElement = event.target as HTMLInputElement;
+      const terminoDeBusqueda = inputElement.value;
+      this.terminosDeBusqueda.push(terminoDeBusqueda);
+      const resultadoElement = document.getElementById('resultado');
+      if (resultadoElement) {
+        resultadoElement.textContent = 'Has escrito: ' + this.terminosDeBusqueda.join(', '); // Muestra los términos de búsqueda en la pantalla
+      }
+      inputElement.value = ''; // Limpia el campo de búsqueda
+    }
+  }
+
+  buscarPorClic() {
+    const inputElement = document.querySelector('input[placeholder="Buscar"]') as HTMLInputElement;
+    const terminoDeBusqueda = inputElement.value;
+    this.terminosDeBusqueda.push(terminoDeBusqueda);
+    const resultadoElement = document.getElementById('resultado');
+    if (resultadoElement) {
+      resultadoElement.textContent = 'Has escrito: ' + this.terminosDeBusqueda.join(', '); // Muestra los términos de búsqueda en la pantalla
+    }
+    inputElement.value = ''; // Limpia el campo de búsqueda
+  }
+
 
 }
